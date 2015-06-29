2b06ae024723c1b74fdf5ad08dc4ff13d603a829
nocmnt <shada.kaalo@gmail.com>
2015-06-29 19:00:52 +0600

    Formatting
    No code changed

--- src/com/neet/DiamondHunter/Main/Game.java
+++ src/com/neet/DiamondHunter/Main/Game.java
@@ -6,21 +6,23 @@ package com.neet.DiamondHunter.Main;
 
 import javax.swing.JFrame;
 
-public class Game {
-	
-	public static void main(String[] args) {
-		
-		JFrame window = new JFrame("Diamond Hunter");
-		
-		window.add(new GamePanel());
-		
-		window.setResizable(false);
-		window.pack();
-		
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
-	
+public class Game
+{
+
+    public static void main(String[] args)
+    {
+
+	JFrame window = new JFrame("Not Sure Yet");
+
+	window.add(new GamePanel());
+
+	window.setResizable(false);
+	window.pack();
+
+	window.setLocationRelativeTo(null);
+	window.setVisible(true);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
 }
